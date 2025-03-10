0a13c259b64f4b74b3389437a38595ee0684d0c0
Borkot ullah <ak.borkotullah@gmail.com>
2025-03-10 22:56:06 +0600

    car doctor server with jwt

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const cookieParser = require("cookie-parser")
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 require('dotenv').config()
 
 // middle ware
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}))
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -20,6 +26,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const logger = (req, res, next) => {
+  console.log(`http://${req.hostname}:${port}${req.originalUrl}`)
+  next()
+}
+
+const verifyToken = (req, res, next) => {
+  console.log("verify cookies",req.cookies)
+  console.log(req.user)
+  const token = req.cookies?.cookie;
+  if (!token) {
+    return res.status(401).send({message: "unauthorized"})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+    if (err) {
+      return res.status(401).send({message: "unauthorized"})
+    }
+    req.user = decoded;
+  });
+  next()
+}
 
 async function run() {
   try {
@@ -35,8 +62,25 @@ async function run() {
 
     app.get('/', (req, res) => res.send('Welcome to Car Doctor server'))
 
-    app.get('/services', async(req, res) => {
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      console.log("user jwt:",user)
+      const Token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      console.log("Token from jwt post route:", Token)
+
+      res
+      .cookie("cookie", Token, {
+        httpOnly: true,
+        secure:true,
+        sameSite: "none"
+      })
+      .send({cookie_success: true})
+      
+    })
+
+    app.get('/services', logger, verifyToken, async(req, res) => {
         const cursor = services.find({})
+        console.log("service route cookies:",req.cookies)
         const result = await cursor.toArray()
       res.send(result)
     })
@@ -51,7 +95,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/bookings', async (req, res)=> {
+    app.get('/bookings', logger, verifyToken, async (req, res)=> {
+      console.log("booking cookies:",req.cookies)
+      console.log("Bookings user", req.user)
+      if (req.user?.email !== req.query?.email) {
+        return res.status(401).send({message: "unauthorized"})
+      }
       let query = {}
       if (req.query?.email) {
         query = {email: req.query.email}
